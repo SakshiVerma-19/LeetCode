64d4e750d452b15a043a09fd1639a5041777c8e0
Sakshi <sakshi.23aiml@cambridge.edu.in>
2025-02-24 23:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.50%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.py
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -1,9 +1,4 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        temp=[]
-        kelvin = celsius+273.15
-        temp.append(kelvin)
-        faren = celsius *1.80+32.00
-        temp.append(faren)
-        return temp
+        return [celsius + 273.15, celsius * 1.8 + 32.0]
          
